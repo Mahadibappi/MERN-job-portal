a90ea0984a23bb505d330ceb3d670a99308cc1fc
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-04-28 12:29:50 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import Home from "./components/home/Home.js";
+import Navbar from "./components/nav/Navbar.js";
 
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">hello projects</h1>
+      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-3xl text-orange-400 text-center">
+      hey this is home Home
+    </div>
+  );
+};
+
+export default Home;
